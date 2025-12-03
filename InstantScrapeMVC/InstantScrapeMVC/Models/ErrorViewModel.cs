217942acc9a7184467cc998fbbc8174d6077ee18
ShahManil17 <manil.shah.2024@gmail.com>
2025-12-03 14:37:41 +0530

    Projet structure created for InstantScrapeMVC

---
+++ InstantScrapeMVC/InstantScrapeMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InstantScrapeMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
